7052585efc9b7af770dfc7252d86c4207a401c53
vncsmcd <151754230+vncsmcd@users.noreply.github.com>
2023-12-06 11:13:33 -0500

    Add files via upload (#2)

--- fundamentals.c
+++ fundamentals.c
@@ -33,4 +33,20 @@ void fundamentals(void) {
     } while (strcmp(buffer1, "q") != 0); // Loops until the user inputs 'q' to quit
 
     printf("*** End of Indexing Strings Demo ***\n\n"); // Prints the end of the indexing strings demonstration
+
+
+    //V2
+    printf("*** Start of Measuring Strings Demo ***\n");
+    char buffer2[BUFFER_SIZE];
+    do {    // Prompts the user for a string then calculates the length and displays it until q key is pressed to exit.
+        printf("Type a string (q- to quit):\n");
+        fgets(buffer2, BUFFER_SIZE, stdin);
+        buffer2[strlen(buffer2) - 1] = '\0';
+        if (strcmp(buffer2, "q") != 0)
+        {
+            printf("The length of \'%s\' is %d characters\n",
+                buffer2, (int)strlen(buffer2));
+        }
+    } while (strcmp(buffer2, "q") != 0);
+    printf("*** End of Measuring Strings Demo ***\n\n");
 }
